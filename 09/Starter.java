3ae6fb64acbd68173461ab18116b3d2174e1fb4f
Krefeld <60035444+Krefeld@users.noreply.github.com>
2022-01-13 17:42:23 +0100

    Create Starter.java

---
+++ 09/Starter.java
@@ -0,0 +1,25 @@
+public class Starter {
+    public static void main(String[] args) {
+        /*
+        neue Methoden: 
+        int size();
+            gibt die Länge des Stacks wieder
+        
+        void show();
+            gibt den Stack aus
+        
+        Object get(int Index);
+            gibt das Element im Stack am Index aus.
+        */
+        
+        Stack a = new Stack();
+        int arr[] = {1,2,3,4,5,6,7,8,9,10};
+        for(int i : arr){
+            a.push(i);
+        }
+        a.show();
+        System.out.println();
+        a.pop();
+        a.show();
+    }
+}
